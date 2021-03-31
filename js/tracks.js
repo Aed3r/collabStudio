68279519812c8bddb92318184cdb500170444d7b
Aed3r <xxgoffi22xx@gmail.com>
2021-03-31 17:50:14 +0200

    fix css

--- js/tracks.js
+++ js/tracks.js
@@ -18,3 +18,30 @@ for (i = 0; i < 500; i++) {
 }
 
 // Met en place l'affichage des temps et mesures
+
+var headersOnLeft = true; // Flag tenant compte si on se trouve au début des pistes
+function drawMeasures() {
+    yPos = tracks.scrollLeft; // Pixels horizontales défilé
+
+    // Activation/désactivation de l'ombre
+    if (yPos > 0 && headersOnLeft) {
+        // On affiche l'ombre
+        headersOnLeft = false;
+        document.querySelectorAll(".trackHeader").forEach(header => {
+            header.style.boxShadow = "10px 20px 30px darkslategray";
+        });
+    } else if (yPos == 0 && !headersOnLeft) {
+        // On enlève l'ombre
+        headersOnLeft = true;
+        document.querySelectorAll(".trackHeader").forEach(header => {
+            header.style.boxShadow = "";
+        });
+    }
+    
+    // Dessine les mesures sur la piste suivant le défilement horizontal
+    document.querySelectorAll(".trackCanvas").forEach(canvas => {
+        const ctx = canvas.getContext('2d');
+
+        for (i = 0; i < )
+    });
+}
