2c747f07d1f100a74612e10424d56592d129b64e
Paul-Antoine Bernard <paul.antoine.bernard7@gmail.com>
2021-05-13 16:02:05 +0200

    ajout des annotations

--- src/users/Servlet_Connexion.java
+++ src/users/Servlet_Connexion.java
@@ -3,11 +3,13 @@ package users;
 import java.io.IOException;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+@WebServlet("/Servlet_Connexion")
 public class Servlet_Connexion extends HttpServlet {
 	public void doGet( HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		this.getServletContext().getRequestDispatcher("/connexion.jsp").forward(request, response);
